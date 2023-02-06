06a943a3a323225ff927fc5189b3e7f035eb9997
Fernando Randi <110851277+RandiSKT@users.noreply.github.com>
2023-02-05 21:48:46 -0300

    Update Previsão.py

--- Previsão.py
+++ Previsão.py
@@ -1,16 +1,33 @@
 import pandas as pd
 from datetime import datetime
 from sklearn.linear_model import LinearRegression
+import warnings
+warnings.filterwarnings("ignore")
 
-# Leitura dos dados que vão ser utilziados para treinar o modelo
+# Ler dos dados que vão ser utilziados para treinar o modelo
+
 df = pd.read_csv('vendas.csv', sep=';')
 df['Data'] = pd.date_range(start="2022-12-06",end="2023-01-20")
-# Leitura dos dados onde vão ser armazenadas as previsões
+
+# Ler dos dados onde serão armazenadas as previsões
+
 prev = pd.read_csv('predict.csv')
 prev['Data'] = pd.date_range(start="2023-01-21",end="2023-01-25")
 
+# Criar uma coluna sequencial para refletir a pasagem do tempo
 
-# ajustando o período atípico de entre 23/12 e 01/01.
+df['sequencia'] = 0
+for n in range(46):
+    df['sequencia'][n] = n
+    
+prev['sequencia'] = 0
+x=0
+for n in range(46, 51):
+    prev['sequencia'][x] = n
+    x+=1
+
+
+# Ajustar o período atípico de entre 23/12 e 01/01.
 
 df['Vendas_Ajust'] = df.Vendas
 
@@ -20,7 +37,7 @@ for n in range(0,7):
 for n in range(0,3):
     df.at[24+n,'Vendas_Ajust'] = df.at[10+n,'Vendas']
 
-# Calculando os fatores sazonais para os dias da semana.
+# Calcular os fatores sazonais para os dias da semana.
 
 df['Fator'] = df['Vendas_Ajust'] / df['Vendas_Ajust'].mean()
 df['dia_da_semana'] = df['Data'].dt.dayofweek
@@ -28,31 +45,29 @@ df_fatores = df.groupby(by='dia_da_semana').mean()
 df = pd.merge(df, df_fatores['Fator'], how = 'outer', on = 'dia_da_semana')
 df = df.sort_values(by=['Data'])
 
-# Apurando as vendas sem a sazonalidade diária.
+# Apurar as vendas sem a sazonalidade diária.
 
 df['Vendas_sem_Saz'] = df['Vendas'] / df['Fator_y']
 
-# Criando uma coluna compativel com o modelo e com o DataFrame de dados
-prev['dia_da_semana'] = prev['Data'].dt.dayofweek
+# Redimensionar arrays para compatibilidade com modelo
+X = df['sequencia'].values.reshape(-1, 1)
+y = df['Vendas'].values.reshape(-1, 1)
+predict = prev['sequencia'].values.reshape(-1, 1)
 
-# Redimensionando arrays para compatibilidade com modelo
-X = df['dia_da_semana'].values.reshape(-1, 1)
-y = df['Vendas_sem_Saz'].values.reshape(-1, 1)
-predict = prev['dia_da_semana'].values.reshape(-1, 1)
 
-# Estabelecendo o modelo e colando os resultados na coluna de vendas do segundo DataFrame
+# Estabelecer o modelo e colar os resultados na coluna de vendas do segundo DataFrame
 model = LinearRegression()
 model.fit(X, y)
 prev['Vendas'] = model.predict(predict)
 
-# Ajustando sazonalidade diária
-x=0
+# Ajustar a sazonalidade diária
+y=0
 for i in range(-7, -3):
-    prev['Vendas'][x] = round(prev['Vendas'][x] * df.iloc[i]['Fator_y'])
-    x+=1
+    prev['Vendas'][y] = round(prev['Vendas'][y] * df.iloc[i]['Fator_y'])
+    y+=1
 
 
-# Mostrando os resultados arredondando as vendas para o número inteiro mais proximo
+# Mostrar os resultados arredondando as vendas para o número inteiro mais proximo
 print('             ***Projeção de Vendas para 5 dias ***' , end='\n\n')
 print('Modelo: Regressão Linear, com ajuste para a sazonalidade diária.', end='\n\n', flush=True)
 day = 21
